b4e982e601316ca37bd358e262668c77b6d39594
zhangxiang <me_zhangxiang@163.com>
2018-09-03 18:11:04 +0800

    数据动态化

--- pages/regionList/regionList.js
+++ pages/regionList/regionList.js
@@ -1,23 +1,78 @@
 // pages/regionList/regionList.js
-Component({
+const http=require("../../utils/http.js")
+Page({
   /**
-   * 组件的属性列表
-   */
-  properties: {
-
-  },
-
-  /**
-   * 组件的初始数据
+   * 页面的初始数据
    */
   data: {
+    goodsList:[]//用来渲染该页面的产品数据
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    console.log(options.id)//url传来的id
+      http.request({
+        apiName: 'activity/activityShoppingList',
+        method: 'post',
+        data: { 'id': options.id, 'currentPage': 1, 'pageSize': 10 },
+        isShowProgress: true,
+        success: (res) => {
+          this.setData({
+            goodsList: res.records,
+          })
+        }
+      })
+      
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
 
   },
 
   /**
-   * 组件的方法列表
+   * 生命周期函数--监听页面卸载
    */
-  methods: {
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
 
   }
+
 })
